8aaf1f64fba9bb35965b9e9630be6ab28ca7522e
dueltech <ygodueltech@gmail.com>
2021-05-26 02:08:23 -0700

    bugfix:winners were swapped!!

--- aitourney.sh
+++ aitourney.sh
@@ -7,19 +7,17 @@ cd "$WINDBOW_HOME" || exit
 
 i=0
 for deck1 in "$WINDBOW_HOME"/Decks/*.ydk; do
-
     deck1="$(basename $deck1 .ydk | sed -e 's/AI_//')"
+
     for deck2 in "$WINDBOW_HOME"/Decks/*.ydk; do
-
-
         deck2="$(basename $deck2 .ydk | sed -e 's/AI_//')"
 
         # ignore mirror match
         if [[ "$deck1" != "$deck2" ]]; then
             "$YGOSHARP_HOME"/YGOSharp/bin/Debug/YGOSharp.exe  > /dev/null 2>&1 &
 
-            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Host=127.0.0.1 Port=7911 Deck="$deck1" > /dev/null 2>&1 &
-            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Host=127.0.0.1 Port=7911 Debug=True Deck="$deck2" > duel.log
+            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck1" Host=127.0.0.1 Port=7911 Deck="$deck1" > /dev/null 2>&1 &
+            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck2" Host=127.0.0.1 Port=7911 Debug=True Deck="$deck2" > duel.log
 
             IS_WINNER="$(grep 'result: Win' duel.log)"
         else
@@ -27,9 +25,9 @@ for deck1 in "$WINDBOW_HOME"/Decks/*.ydk; do
         fi
 
         if [[ -z "$IS_WINNER" ]];then
-            WINNER="$deck2"
-        else
             WINNER="$deck1"
+        else
+            WINNER="$deck2"
         fi
         NOW=$(date +"%Y-%m-%d %H:%M:%S")
         echo "$i,$NOW,$deck1,$deck2,$WINNER"
