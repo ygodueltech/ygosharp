8457840ee13f0d13325aaf0723ab6b67063f3b4e
dueltech <ygodueltech@gmail.com>
2021-05-26 19:03:00 -0700

    failed attempt to fix shell script version

--- aitourney.sh
+++ aitourney.sh
@@ -2,13 +2,14 @@ YGOSHARP_HOME="$HOME/workspace/ygo/ygosharp"
 WINDBOW_HOME="$HOME/workspace/ygo/windbot"
 
 
-
 cd "$WINDBOW_HOME" || exit
+mkdir -p tourney
 
 i=0
 for deck1 in "$WINDBOW_HOME"/Decks/*.ydk; do
     deck1="$(basename $deck1 .ydk | sed -e 's/AI_//')"
 
+    #deck1=Dragun
     for deck2 in "$WINDBOW_HOME"/Decks/*.ydk; do
         deck2="$(basename $deck2 .ydk | sed -e 's/AI_//')"
 
@@ -16,18 +17,21 @@ for deck1 in "$WINDBOW_HOME"/Decks/*.ydk; do
         if [[ "$deck1" != "$deck2" ]]; then
             "$YGOSHARP_HOME"/YGOSharp/bin/Debug/YGOSharp.exe  > /dev/null 2>&1 &
 
-            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck1" Host=127.0.0.1 Port=7911 Deck="$deck1" > /dev/null 2>&1 &
-            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck2" Host=127.0.0.1 Port=7911 Debug=True Deck="$deck2" > duel.log
+            #"$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck2" Host=127.0.0.1 Port=7911 Deck="$deck2" Debug=True > /dev/null 2>&1 &
+            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck2" Host=127.0.0.1 Port=7911 Deck="$deck2" Debug=True > duel2.log &
+            "$WINDBOW_HOME"/bin/Debug/WindBot.exe Name="$deck1" Host=127.0.0.1 Port=7911 Deck="$deck1" Debug=True > duel.log
 
-            IS_WINNER="$(grep 'result: Win' duel.log)"
+            D1_WINS="$(grep 'result: Win' duel.log)"
+            cp duel.log tourney/"$deck1"_vs_"$deck2".log
+            cp duel2.log tourney/"$deck1"_vs_"$deck2"_2.log
         else
             IS_WINNER="$deck2"
         fi
 
-        if [[ -z "$IS_WINNER" ]];then
-            WINNER="$deck1"
-        else
+        if [[ -z "$D1_WINS" ]];then
             WINNER="$deck2"
+        else
+            WINNER="$deck1"
         fi
         NOW=$(date +"%Y-%m-%d %H:%M:%S")
         echo "$i,$NOW,$deck1,$deck2,$WINNER"
